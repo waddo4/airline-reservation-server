51cd1894b6db4fdd42e07eed6c29a97cf1de8901
waddo4 <109324137+waddo4@users.noreply.github.com>
2023-02-08 01:59:09 +1100

    users table password fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+User.create :name => 'jonesy', :email => 'jonesy@ga.co', :password => 'chicken', :admin => true
+User.create :email => 'sicklad@ga.co', :password => 'chicken'
+User.create :email => 'craigsy@ga.co', :password => 'chicken'
+puts "#{ User.count } users"
