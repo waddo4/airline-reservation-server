0f85afd20df91360b9bdf3d4c6087d2bf1244e3b
waddo4 <109324137+waddo4@users.noreply.github.com>
2023-02-07 18:25:36 +1100

    user table updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_07_060544) do
+ActiveRecord::Schema.define(version: 2023_02_07_072242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2023_02_07_060544) do
   create_table "users", force: :cascade do |t|
     t.text "name"
     t.text "email"
-    t.integer "password"
+    t.text "password"
     t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
